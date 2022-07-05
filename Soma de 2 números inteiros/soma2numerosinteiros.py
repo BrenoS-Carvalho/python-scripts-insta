c290d467cedf1b9f5578fa2a78dca5348626536c
Breno Carvalho <104074118+BrenoS-Carvalho@users.noreply.github.com>
2022-07-05 10:47:03 -0300

    Finalizando...

---
+++ Soma de 2 números inteiros/soma2numerosinteiros.py
@@ -0,0 +1,3 @@
+primeiro_numero = int(input('Digite o primeiro número: '))
+segundo_numero = int(input('Digite o segundo número: '))
+print(f'A soma entre {primeiro_numero} e {segundo_numero} é {primeiro_numero + segundo_numero}')
