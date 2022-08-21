94b2fe13922a0c54bbb3f540eab05b1aac063010
Breno Carvalho <104074118+BrenoS-Carvalho@users.noreply.github.com>
2022-08-21 16:34:43 -0300

    Contagem_regressiva

---
+++ Contagem Regressiva/main.py
@@ -0,0 +1,9 @@
+import time
+print('='*30)
+print('Contagem Regressiva Usando Python')
+print('='*30)
+for contagem_regressiva in range(10,0,-1):
+    print(contagem_regressiva)
+    time.sleep(1)
+print('='*30)
+print('Fim da contagem')
