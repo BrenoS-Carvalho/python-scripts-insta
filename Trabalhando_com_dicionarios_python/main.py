6e8d4318e91ffcfd484f6c502354167dbc408d5d
Breno Carvalho <104074118+BrenoS-Carvalho@users.noreply.github.com>
2022-10-08 06:31:25 -0300

    Dicionários

---
+++ Trabalhando_com_dicionarios_python/main.py
@@ -0,0 +1,5 @@
+dicionario = {'nome':'Breno', 'idade': 19, 'estado': 'Minas Gerais'}
+print('='*30)
+print('='*30)
+print(f'O {dicionario["nome"]} tem {dicionario["idade"]} anos e nasceu em {dicionario["estado"]}.')
+print('='*30)
